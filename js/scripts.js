366e040003fe131666ac4dda14b171262d41c293
Mitzi Hazel Tizon <mitzihazel_15@yahoo.com>
2016-06-24 17:33:56 +0800

    Updated Food Diary Design

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-(function ($) {
-	$('a[href="#today"]').click(function () {
-	  $("div#today").hide();
-	});
-	$('a[href="#journal"]').click(function () {
-	  $("div#today").show();
-	});
-});
+$(document).ready(function(){
+    $('a[href="#today"]').click(function () {
+      $("div#today").show();
+    });
+    $('a[href="#journal"]').click(function () {
+      $("div#today").hide();
+    });
+}); 
